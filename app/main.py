5f1220c81179c19a7f619b06eb195fd47072c895
yulian-khalitov <khalitov.yulian@yandex.ru>
2021-09-28 13:55:40 +0300

    api: describe response model

--- app/main.py
+++ app/main.py
@@ -2,7 +2,7 @@ import requests
 import aioredis
 from fastapi import FastAPI
 from pydantic import BaseModel
-from typing import List
+apifrom typing import List, Dict
 from app.settings import PANCAKESWAP_API_URL
 from app.utils import calculate_lp_token_price
 
@@ -11,17 +11,20 @@ class Payload(BaseModel):
     addresses: List[str]
 
 
+class Response(BaseModel):
+    prices: Dict[str, float]
+
+
 app = FastAPI()
 
 
-@app.post('/')
-async def main(payload: Payload):
+@app.post('/', response_model=Response)
+async def prices(payload: Payload):
     redis = await aioredis.from_url("redis://redis:6379", encoding="utf-8", decode_responses=True)
     result = {}
     for address in payload.addresses:
         price = await redis.get(address)
         if not price:
-            print('write')
             response = requests.get(PANCAKESWAP_API_URL.format(address=address)).json()
             symbol = response['data']['symbol']
             if symbol != 'Cake-LP':
@@ -30,9 +33,7 @@ async def main(payload: Payload):
                 price = calculate_lp_token_price(address)
 
             await redis.set(address, price, ex=60)
-        else:
-            print('cache')
 
         result[address] = price
 
-    return result
+    return {'prices': result}
